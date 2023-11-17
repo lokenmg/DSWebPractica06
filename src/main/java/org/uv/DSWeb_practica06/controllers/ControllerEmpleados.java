bf08d6eb566eac6aa1ece1709858ba2a1ddc5b73
Itzelll <mmitzel2435@gmail.com>
2023-11-16 18:40:52 -0600

    update imports

--- src/main/java/org/uv/DSWeb_practica06/controllers/ControllerEmpleados.java
+++ src/main/java/org/uv/DSWeb_practica06/controllers/ControllerEmpleados.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/RestController.java to edit this template
  */
-package org.uv.DSWeb_Practica06.controllers;
+package org.uv.DSWeb_practica06.controllers;
 
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RestController;
